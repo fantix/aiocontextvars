a41b928fa143e216730007fcc0e1a7673352a8bb
Fantix King <fantix.king@gmail.com>
2017-12-03 16:02:00 +0800

    Re-implement inherit, added ``create_task`` shortcut

--- aiocontextvars/__init__.py
+++ aiocontextvars/__init__.py
@@ -5,7 +5,8 @@ __email__ = 'fantix.king@gmail.com'
 __version__ = '0.1.0'
 
 from .context import Context
-from .inherit import enable_inherit, disable_inherit
+from .inherit import enable_inherit, disable_inherit, create_task
 from .var import ContextVar
 
-__all__ = ('ContextVar', 'Context', 'enable_inherit', 'disable_inherit')
+__all__ = ('ContextVar', 'Context', 'enable_inherit', 'disable_inherit',
+           'create_task')
--- aiocontextvars/inherit.py
+++ aiocontextvars/inherit.py
@@ -2,50 +2,54 @@ import asyncio
 
 from .context import Context
 
-_original_task_factories = {}
 
-
-def _task_factory(loop, coro):
-    loop.set_task_factory(_original_task_factories.get(loop))
-    try:
-        task = loop.create_task(coro)
-
-        if getattr(task, '_source_traceback', None):
-            del getattr(task, '_source_traceback')[-2:]
-
-        task.ctx = Context.current().copy()
-
-        return task
-    finally:
-        loop.set_task_factory(_task_factory)
-
-
-class InheritContext:
-
-    __slots__ = ('_loop',)
+class TaskFactory:
+    __slots__ = ('_loop', '_orig_factory')
 
     def __init__(self, loop):
         self._loop = loop
+        self._orig_factory = loop.get_task_factory()
+        self._loop.set_task_factory(self)
+
+    def uninstall(self):
+        self._loop.set_task_factory(self._orig_factory)
 
     def __enter__(self):
         pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        disable_inherit(self._loop)
+        self.uninstall()
+
+    def __call__(self, loop, coro):
+        assert loop is self._loop
+        loop.set_task_factory(self._orig_factory)
+        try:
+            task = loop.create_task(coro)
+
+            if getattr(task, '_source_traceback', None):
+                del getattr(task, '_source_traceback')[-2:]
+
+            task.ctx = Context(Context.current())
+
+            return task
+        finally:
+            loop.set_task_factory(self)
 
 
 def enable_inherit(loop=None):
     if loop is None:
         loop = asyncio.get_event_loop()
-    if loop in _original_task_factories:
-        return
-    _original_task_factories[loop] = loop.get_task_factory()
-    loop.set_task_factory(_task_factory)
-    return InheritContext(loop)
+    return TaskFactory(loop)
 
 
 def disable_inherit(loop=None):
     if loop is None:
         loop = asyncio.get_event_loop()
-    if loop in _original_task_factories:
-        loop.set_task_factory(_original_task_factories.pop(loop))
+    getattr(loop.get_task_factory(), 'uninstall', lambda: None)()
+
+
+def create_task(coro, *, loop=None):
+    if loop is None:
+        loop = asyncio.get_event_loop()
+    with enable_inherit(loop):
+        return loop.create_task(coro)
