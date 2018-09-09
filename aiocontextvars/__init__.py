b00232810854a05ea20ff67d62726a86dbf4713c
Fantix King <fantix.king@gmail.com>
2018-09-09 11:50:37 +0800

    Bump version: 0.1.2 → 0.2.0

--- aiocontextvars/__init__.py
+++ aiocontextvars/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Fantix King"""
 __email__ = 'fantix.king@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.2.0'
 
 import sys
 
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='aiocontextvars',
-    version='0.1.2',
+    version='0.2.0',
     description="PEP-550 polyfill, execution context for asyncio.",
     long_description=readme + '\n\n' + history,
     author="Fantix King",
